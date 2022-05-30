450fe111bfa92ab0035fb5871673e7a7d41708c4
sunericd <edsun@DESKTOP-5BCOF0K.localdomain>
2022-05-29 20:30:56 -0700

    add init

---
+++ dynamicviz/__init__.py
@@ -0,0 +1,3 @@
+'''DynamicViz provides a wrapper for generating and interpreting dynamic visualizations from traditional static dimensionality reduction visualization methods'''
+
+__version__ = "0.0.1"
